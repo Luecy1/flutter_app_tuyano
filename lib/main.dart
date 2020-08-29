15e99e38378741bd99365f0623db79649645a916
Youhei Oonuma <oonuma617@gmail.com>
2020-08-29 19:33:44 +0900

    Rss List Page

--- lib/main.dart
+++ lib/main.dart
@@ -27,19 +27,43 @@ class RssListPage extends StatelessWidget {
         title: Text('RSS'),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-          ],
+        child: ListView.builder(
+          itemCount: Rss.rssList.length,
+          padding: EdgeInsets.all(10.0),
+          itemBuilder: _buildRssItem,
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
     );
+  }
+
+  Widget _buildRssItem(BuildContext context, int index) {
+    final rss = Rss.rssList[index];
+    return ListTile(
+      contentPadding: EdgeInsets.all(10.0),
+      title: Text(
+        rss.name,
+        style: TextStyle(fontSize: 24.0),
+      ),
+      onTap: () {
+        final snackBar = SnackBar(content: Text('tap ${rss.name}'));
+        Scaffold.of(context).showSnackBar(snackBar);
+      },
+    );
+  }
+}
+
+class Rss {
+  final String name;
+  final String link;
+
+  Rss(this.name, this.link);
+
+  static List<Rss> get rssList {
+    return [
+      Rss('主要ニュース', 'https://news.yahoo.co.jp/pickup/rss.xml'),
+      Rss('国際情勢', 'https://news.yahoo.co.jp/pickup/world/rss.xml'),
+      Rss('国内の出来事', 'https://news.yahoo.co.jp/pickup/domestic/rss.xml'),
+      Rss('IT関係', 'https://news.yahoo.co.jp/pickup/computer/rss.xml'),
+    ];
   }
 }
